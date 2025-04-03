29e80633c912c64c7bbecd6ff6f52accd2c08b5b
eplitwabot <anothereplitbotwa@gmail.com>
2025-04-03 12:07:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MOZART+B0T",
+  botOwner: 'Luisen-Typ', // Your name
+  ownerNumber: '+4917672415053', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  description: "Dieser Bot existiert nur um Leute zu nerven.",
+  version: "gestern",
 };
 
 module.exports = settings;
